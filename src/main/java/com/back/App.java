b45471105e8b8ac8c262e6180430f1814a44a248
jueunk617 <juuuune62@naver.com>
2025-05-29 10:45:15 +0900

    feat: SystemController 도입.

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -1,24 +1,31 @@
 package com.back;
 
+import com.back.domain.system.controller.SystemController;
 import com.back.domain.wiseSaying.controller.WiseSayingController;
 
 import java.util.Scanner;
 
 public class App {
     void run() {
+        System.out.println("== 명언 앱 ==");
+
         Scanner scanner = new Scanner(System.in);
         WiseSayingController wiseSayingController = new WiseSayingController(scanner);
-
-        System.out.println("== 명언 앱 ==");
+        SystemController systemController = new SystemController();
 
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine().trim();
-
-            if (cmd.equals("종료")) break;
-
             Rq rq = new Rq(cmd);
+
+            if (cmd.equals("종료")) {
+                systemController.actionExit();
+                break;
+            }
+
             wiseSayingController.handleCommand(rq);
         }
+
+        scanner.close();
     }
 }
